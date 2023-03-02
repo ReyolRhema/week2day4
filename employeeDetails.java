e69dcaf239e0ce02bb94e9c520794eb6150532f6
ReyolRhema <126499554+ReyolRhema@users.noreply.github.com>
2023-03-02 21:59:15 +0530

    Add files via upload

---
+++ employeeDetails.java
@@ -0,0 +1,81 @@
+package week2day3;
+
+public class employeeDetails {
+		public void employeName(String empName , int empId) {
+		System.out.println(empName +empId);
+		}
+		public void employeAddress(String empAddress) {
+			System.out.println(empAddress);
+		}
+		public void employeSalary(double empsalary) {
+			System.out.println(empsalary);
+		}
+		public void employeMobileno(long empno) {
+			System.out.println(empno);
+		}
+		public static void main(String[] args) {
+			employeeDetails myemp =new employeeDetails() ;
+			myemp.employeName("reyol", 1265);
+			myemp.employeAddress("poonamalle");
+			myemp.employeSalary(100000.90);
+			myemp.employeMobileno(123456789l);
+		}
+}
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
+			
+			
+			
+		
+		
+			
+			  
+	
