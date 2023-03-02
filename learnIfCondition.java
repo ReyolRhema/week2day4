5043c8de4962deea44314cf0f9d1dbafa63f4a6e
ReyolRhema <126499554+ReyolRhema@users.noreply.github.com>
2023-03-02 22:02:54 +0530

    Add files via upload

---
+++ learnIfCondition.java
@@ -0,0 +1,26 @@
+package week2day4;
+
+
+
+public class learnIfCondition {
+	public static void main(String[] args) {
+		int a=-5; 
+		if (a<0){
+			System.out.println(" positive");
+		}else  {
+			System.out.println("negative");}
+			
+		for(int i=1; i<5;i++) {
+			System.out.println(i);
+			}
+		
+		for (int i= 21; i >3; i--) {
+			System.out.println(i);
+	
+			
+			
+		}
+		}
+	}
+
+
